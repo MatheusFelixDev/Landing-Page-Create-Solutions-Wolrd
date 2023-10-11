877b7a74c6c7cff6cee6f27341fb18f3ab1a7291
MatheusFelixDev <MatheusFelix.Dev@gmail.com>
2023-10-11 18:36:27 -0300

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from './components/ui/button';
+
+export function App() {
+  return(
+    <>
+      <h1>O sangue de Jesus tem poder</h1>
+      <Button>Amém</Button>
+    </>
+  )
+}
