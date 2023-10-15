bba5681faf0fb978eecf59d7d91ade20b991443e
MatheusFelixDev <MatheusFelix.Dev@gmail.com>
2023-10-15 07:39:14 -0300

    edit navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -9,7 +9,13 @@ export function NavBar(){
     };
     return (
         <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-        <h1 className='w-full text-3xl font-bold text-green-500'>CreateSolutionsWolrd.</h1>
+        <h1 className='w-full text-3xl font-bold text-green-500'>
+                {nav ? (
+                    <span className="hidden">CreateSolutionsWolrd.</span>
+                ) : (
+                    'CreateSolutionsWolrd.'
+                )}
+            </h1>
         <ul className='hidden md:flex '>
                 <li className='p-4 transition-colors hover:scale-105 duration-300 hover:text-[#00df9a]'>
                     <a href='#'>
@@ -45,9 +51,9 @@ export function NavBar(){
             <div onClick={handleNav} className='block md:hidden'>
                 {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
             </div>
-
+            
             <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-black ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
-                <h1 className='w-full text-3xl font-bold text-green-500 m-4'>CrateSolutionsWolrd.</h1>
+                
 
                 <li className='p-4 border-b-4 border-gray-600 transition-colors hover:scale-105 duration-300 hover:text-[#00df9a]'>
                     <a href='#'
