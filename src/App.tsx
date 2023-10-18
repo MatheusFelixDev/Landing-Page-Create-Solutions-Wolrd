51f47f64e802db7e2901531a07cc51215c1ec847
MatheusFelixDev <MatheusFelix.Dev@gmail.com>
2023-10-18 11:13:32 -0300

    add Newsletter

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner'
 import { Button } from './components/ui/button';
 import { Service } from './components/service';
+import { Newsletter } from './components/Newsletter';
 
 export function App() {
   return(
@@ -9,6 +10,7 @@ export function App() {
       <NavBar />
       <Banner />
       <Service />
+      <Newsletter />
       <h1>O sangue de Jesus tem poder</h1>
       <Button>Amém</Button>
     </>
---
+++ src/components/Newsletter.tsx
@@ -0,0 +1,30 @@
+import { Button } from "./ui/button";
+import { Input } from "@/components/ui/input"
+
+export function Newsletter(){
+    return (
+
+        <section id="tips">
+            <div className='w-full py-16 text-white px-4'>
+                <div className='max-w-[1240px] mx-auto grid lg:grid-cols-3'>
+                    <div className='lg:col-span-2 my-4'>
+                        <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>
+                            Quer dicas para otimizar seu fluxo?
+                        </h1>
+                        <p>Assine nosso boletim de noticias e mantenha-se atualizado. É de GRAÇA!!!</p>
+                    </div>
+                    <div className='my-4 '>
+                        <div className="flex w-full max-w-sm items-center space-x-2">
+                            <Input type="email" placeholder="Email" />
+                            <Button type="submit">Subscribe</Button>
+                        </div>
+                        <p className="my-2">
+                            Nós nos preocupamos com a proteção de seus dados. Leia nossa    
+                            <Button variant="link">Política de Privacidade .</Button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
